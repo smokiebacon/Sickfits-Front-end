9669a62b6c8b84a2b38b3b4460db579c9e6bf9c3
Abraham Hsu <smokiebacon@gmail.com>
2020-05-31 23:28:43 -0700

    adds styled-components, Header, Meta, Nav and Page

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Home = (props) => (
+  <div>
+    <h1>asdasd</h1>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+  </div>
+)
+
+export default Home
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Sell = (props) => (
+  <div>
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+    <h1>Sell</h1>
+  </div>
+)
+
+export default Sell
